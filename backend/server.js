88d4ca7327a3bb92419a23bf302455c87e7b9a83
burukgetu <burukgetu000@gmail.com>
2024-08-15 15:50:09 -0300

    backend part finished

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser"; 
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // to parse the requests with JSON payloads
+app.use(cookieParser()); // to parse cookies for req domain
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
+
+app.get("/", (req,res) => {
+    res.send("hello world!")
+});
+
+
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`server is listening on port ${PORT}`)
+  }
+);
